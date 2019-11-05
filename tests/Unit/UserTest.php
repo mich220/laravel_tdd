aadda9558546741dae51f67fbc350a0e46c5a0fc
mich <msmo.private@gmail.com>
2019-11-05 19:07:53 +0100

    made projects accessible for invited users, tested it

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -2,7 +2,10 @@
 
 namespace Tests\Unit;
 
+use App\User;
 use Illuminate\Database\Eloquent\Collection;
+
+use Facades\Tests\Setup\ProjectFactory;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -20,5 +23,25 @@ class UserTest extends TestCase
         $this->assertInstanceOf(Collection::class, $user->projects);
     }
 
+    /** @test */
 
+    function a_user_has_accessible_projects()
+    {
+        $john = $this->signIn();
+
+        ProjectFactory::ownedBy($john)->create();
+
+        $this->assertCount(1, $john->accessibleProjects());
+
+        $sally = factory(User::class)->create();
+        $nick = factory(User::class)->create();
+
+        $project = tap(ProjectFactory::ownedBy($sally)->create())->invite($nick);
+
+        $this->assertCount(1, $john->accessibleProjects());
+
+        $project->invite($john);
+
+        $this->assertCount(2, $john->accessibleProjects());
+    }
 }
